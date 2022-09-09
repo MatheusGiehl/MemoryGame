b4444ae0354e96c4404f8dfd648597b44b2a6a30
Matheus <giehlmatheus96@gmail.com>
2022-09-09 16:18:57 -0300

    update index.js

---
+++ javascript/index.js
@@ -0,0 +1,39 @@
+const grid = document.querySelector('.grid')
+
+const characters = [
+    'beth',
+    'brain',
+    'jerry',
+    'jessica',
+    'meeseeks',
+    'morty',
+    'pessoa-passaro',
+    'pickle-rick',
+    'rick',
+    'scroopy',
+    'summer'
+];
+
+const createElement = (tag, className) => {
+    const element = document.createElement(tag);
+    element.className = className;
+    return element;
+}
+
+const createCard = () => {
+
+    const card = createElement('div', 'card');
+    const front = createElement('div', 'face front');
+    const back = createElement('div', 'face back');
+
+    card.appendChild(front);
+    card.appendChild(back);
+
+    return card;
+}
+
+const loadGame = () => {
+    characters.forEach((character) => {
+
+    });
+}
